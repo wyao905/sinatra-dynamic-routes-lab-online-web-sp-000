e921b99106a7b285fe87281cb84de616a89ee238
Warren Yao <warrenyca905@gmail.com>
2019-09-13 15:57:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,6 +12,6 @@ class App < Sinatra::Base
   end
   
   get '/say/:number/:phrase' do
-    params[:number].to_i.times{return params[:phrase]}
+    params[:number].to_i * params[:phrase]
   end
 end
