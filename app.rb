3b977cec2d03a428f41f36df597f2feb4acd19bf
Warren Yao <warrenyca905@gmail.com>
2019-09-13 16:02:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,4 +18,7 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word1]} #{params[:word1]} #{params[:word1]} #{params[:word1]}"
   end
+  
+  get '/:operation/:number1/:number2' do
+    
 end
